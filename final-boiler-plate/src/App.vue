4349d3f53d45bc04f4f3ad394eff5b97b61fa807
Adria <adrimadu19@gmail.com>
2022-08-23 13:00:03 +0200

    Log out de sesiones done

--- final-boiler-plate/src/App.vue
+++ final-boiler-plate/src/App.vue
@@ -4,27 +4,45 @@ import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router";
 import { useUserStore } from "./stores/user.js";
 import { ref } from "vue";
-
+import {supabase} from './supabase';
+import store from './stores/index'
 const router = useRouter();
 const userStore = useUserStore();
 const { user } = storeToRefs(userStore);
 
-onMounted(async () => {
-  const appReady = ref(null);
-  try {
-    await userStore.fetchUser(); // here we call fetch user
-    if (!user.value) {
-      // redirect them to logout if the user is not there
-      appReady.value = true;
-      router.push({ path: "/auth/login" });
-    } else {
-      // continue to dashboard
-      // router.push({ path: "/" });
-    }
-  } catch (e) {
-    console.log(e);
-  }
+
+
+const appReady = ref("");
+
+const users = supabase.auth.user();
+
+if (!users) {
+  appReady.value = true;
+}
+
+
+supabase.auth.onAuthStateChange((_, session) => {
+  store.methods.setUser(session);
+  appReady.value = true;
+  
 });
+
+// onMounted(async () => {
+//   const appReady = ref(null);
+//   try {
+//     await userStore.fetchUser(); // here we call fetch user
+//     if (!user.value) {
+//       // redirect them to logout if the user is not there
+//       appReady.value = true;
+//       router.push({ path: "/auth/login" });
+//     } else {
+//       // continue to dashboard
+//       // router.push({ path: "/" });
+//     }
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
 </script>
 
 <template>
